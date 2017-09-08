6c8f78e460ce1935edb5d06f2ac4f5814d0a1363
Ramadan Sali <dansali@users.noreply.github.com>
2017-09-08 10:03:14 +1000

    Update directory

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,7 +9,7 @@
         <title>Jobmatch</title>
 
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" type="text/css" href="../assets/css/style.css">	
+		<link rel="stylesheet" type="text/css" href="../resources/assets/css/style.css">	
     </head>
 	
     <body>
