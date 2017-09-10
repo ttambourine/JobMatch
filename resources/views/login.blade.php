8e810de7da2e5ba15e04bfe16e7df1d9444527c4
BenjaminWalden <s3484942@student.rmit.edu.au>
2017-09-10 20:15:58 +1000

    routes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
-<html >
-<head>
-  <meta charset="UTF-8">
-  <title>Sign-Up/Login Form</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  
-  <link rel="stylesheet" type="text/css" href="../resources/assets/css/style.css">
+        <title>Jobmatch</title>
 
-  
-</head>
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" type="text/css" href="../resources/assets/css/style.css">	
+    </head>
 
 <body>
 <?php include("{$_SERVER['DOCUMENT_ROOT']}/resources/views/header.php");?>
