cf45174cc875384bb0ac6027b12dc79d91fa59ad
BenjaminWalden <s3484942@student.rmit.edu.au>
2017-09-15 10:28:28 +1000

    footer edit

--- resources/views/footer.php
+++ resources/views/footer.php
@@ -12,20 +12,17 @@ echo "
 			</div>
 			<div class='share'>
 				<h5>Submit a Job</h5>
-				<h6>Post a Job
-				</h6>
+				<a href='/createJob' class='footerBot'><h6>Post a Job</h6></a>
 			</div>
 			<div class='share'>
 				<h5>Profile</h5>
 				<h6>Profile <br>
-					Preferences <br>
-					History
+					<a href='/account' class='footerBot'>Preferences <br></a>
 				</h6>
 			</div>
 			<div class='share'>
 				<a href='/about' class='footerTop'><h5>Help</h5></a>
 				<a href='/about' class='footerBot'><h6>About</a> <br>
-					Tutorial <br>
 					<a href='/faq'class='footerBot'>FAQ </a><br>
 					<a href='/contact'class='footerBot'>Contact Us</a>
 				</h6>
