bb7001064f9ce1866fe74e2f5b442189e54032fb
rmit-s3490462-blake-cramer <s3490462@student.rmit.edu.au>
2017-09-17 19:23:04 +1000

    First trial of creating template for selecting a job

---
+++ resources/views/selectJob.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<meta charset="UTF-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+   	<link rel="stylesheet" type="text/css" href="../resources/assets/css/style.css">
+  </head>
+
+<body>  
+	<div class="centre">
+<?php include("{$_SERVER['DOCUMENT_ROOT']}/resources/views/header.php");?>
+	<form action="/" method="post">
+	    <div class="contentformBox">	    
+		<div class="contentform">
+		<h1>Select a Job</h1>	    
+		<div class="contentform">
+			<div class="leftcontact">
+				<div class="form-group">
+					<p>Name</p>	
+					<input type="text" name="title" id="title" required />
+				</div>
+				<div class="form-group">
+					<p>Expertise</p>	
+					<input type="text" name="expertise" id="expertise" required />
+				</div>
+                <div class="form-group">
+                    <p>Location</p>
+                    <input type="text" name="location" id="location" required />
+                </div>
+			</div>
+
+			<div class="rightcontact">	
+				<div class="form-group">
+					<p>Creator</p>
+                    <img src="https://pixabay.com/en/blank-profile-picture-mystery-man-973461/">
+                <div class="profilepic-upload">
+                    <button value="upload">Select Profile Picture</button>    
+                </div>
+					<input type="text" name="location" id="location" required />
+				</div>
+				<div class="form-group">
+					<p>Price</p>	
+					<input type="number" name="price" id="price" required />
+				</div>
+				<div class="form-group">
+					<p>Deadline</p>	
+					<input type="text" name="deadline" id="deadline" required />
+				</div>
+			</div>
+		</div>
+				<div class="centreAccount">
+			<div class="form-group">
+				<p>About the Job<span>    250 characters or less</span></p>
+                <textarea type="text" name="about" id="about" maxlength="250" placeholder="Specifics about the job"></textarea>
+			</div>
+		<br>
+		<input type="submit" value="Save Information" class="submitAccForm">
+		<br>
+		<br>
+		</div>
+		</div>
+		</div>
+	</form>	
+
+  		<?php include("{$_SERVER['DOCUMENT_ROOT']}/resources/views/footer.php");?>
+		</div>
+</body>
+</html>
