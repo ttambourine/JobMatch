0d5d464f25dedd79a674882452f122cd45dba654
rmit-s3490462-blake-cramer <s3490462@student.rmit.edu.au>
2017-09-17 23:06:31 +1000

    testing layouts

--- resources/views/selectJob.blade.php
+++ resources/views/selectJob.blade.php
@@ -23,27 +23,28 @@
 					<p>Expertise</p>	
 					<input type="text" name="expertise" id="expertise" required />
 				</div>
+                <div class="form-group">
+                    <p>Location</p>
+                </div>
 			</div>
 
 			<div class="rightcontact">	
 				<div class="form-group">
-					<p>Location</p>	
+					<p>Price</p>	
 					<input type="text" name="location" id="location" required />
 				</div>
 				<div class="form-group">
-					<p>Price</p>	
+					<p>Deadline</p>	
 					<input type="number" name="price" id="price" required />
 				</div>
 				<div class="form-group">
-					<p>Deadline</p>	
+					<p>About The Job</p>	
 					<input type="text" name="deadline" id="deadline" required />
 				</div>
 			</div>
 		</div>
 				<div class="centreAccount">
 			<div class="form-group">
-				<p>About the Job<span>    250 characters or less</span></p>
-                <textarea type="text" name="about" id="about" maxlength="250" placeholder="Specifics about the job"></textarea>
 			</div>
 		<br>
 		<input type="submit" value="Select Job" class="submitAccForm">
