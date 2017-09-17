c10c405341f4ddfd009c0260f5255a44acf1597c
BenjaminWalden <s3484942@student.rmit.edu.au>
2017-09-17 15:56:43 +1000

    browse

--- resources/views/browse.blade.php
+++ resources/views/browse.blade.php
@@ -28,6 +28,21 @@
 				<h4> Match </h4>
 			</div>
 		</div>
+		<div class="jobTab">
+			<div class="jobImg">
+				<img src="https://albertaventure.com/wp-content/uploads/2014/12/01_bpoy_story01.jpg" width="68" height="68">
+			</div>
+			<div class="jobBody">
+				<h3> Garden Landscaping </h3>
+				<h4> Frankston, Victoria, Australia </h4>
+			</div>
+			<div class="jobEarn">
+				<h1> $50 </h1>
+				<h4> Match </h4>
+			</div>
+		</div>
+		
+		<div class="bink"></div>
 	
 		<?php include("{$_SERVER['DOCUMENT_ROOT']}/resources/views/footer.php");?>
 	</div>	
