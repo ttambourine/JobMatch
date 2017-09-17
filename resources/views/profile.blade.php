eb0ddd5d582062aaba40b1b9aca78a5c56c369c2
BenjaminWalden <s3484942@student.rmit.edu.au>
2017-09-17 13:33:32 +1000

    profile edit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,56 +13,16 @@
 	
     <body>
 	<div class="centre">
-	
 		<?php include("{$_SERVER['DOCUMENT_ROOT']}/resources/views/header.php");?>
-		
-		<div class="centerBox">
-		<div class="leftCol">
-			<img class="profImg" src="https://albertaventure.com/wp-content/uploads/2014/12/01_bpoy_story01.jpg" width="250" height="250px">
-			<h5>Rating:</h5>		<h6 class="fill">image of stars/5	</h6>		<br>
-			<h5>Location:</h5>		<h6 class="fill">Registered Location</h6>		<br>
-			<h5>Jobs Done:</h5>		<h6 class="fill">0					</h6>		<br>
-			<h5>Jobs Posted:</h5>	<h6 class="fill">0					</h6>		<br>
-		</div>
-		<div class="rightCol">
-			<h1> Chris P. Chips</h1>
-			<h3> Qualifications </h3>
-			<h3> Contact </h3>
-			<h3> Qualifications </h3>
-			<h3> Qualifications </h3>
-			<h3> About User </h3>
-		</div>
-		<button class="accordion"><h5 class="faqHead">User Reviews</h5></button>
-			<div class="panel">
-				<div class="review"> 
-					<img class="reviewImg" src="https://c1.staticflickr.com/3/2936/14387367072_85312c31b3_b.jpg">
-					<h5 class="reviewName"> Clair Voyuent </h5>
-					<h6 class="reviewBody"> He was quick and communicated well<h6>
-				</div>
-			</div>
-		</div>
-		
-		<br>
-		<br>
 
+		<div class="thinBanner">
+			<img src="http://i.imgur.com/JKdd1ns.jpg" class="thinBan">
+		</div>
+		<div class="profileImg">
+			<img src="https://albertaventure.com/wp-content/uploads/2014/12/01_bpoy_story01.jpg" class="profileImgPic">
+		</div>
+		
 		<?php include("{$_SERVER['DOCUMENT_ROOT']}/resources/views/footer.php");?>
-				</div>
-		<script>
-		var acc = document.getElementsByClassName("accordion");
-		var i;
-
-		for (i = 0; i < acc.length; i++) {
-			acc[i].onclick = function(){
-				this.classList.toggle("active");
-				var panel = this.nextElementSibling;
-				if (panel.style.display === "block") {
-					panel.style.display = "none";
-				} else {
-					panel.style.display = "block";
-				}
-			}
-		}
-		</script>
-	
+	</div>	
 	</body>
 </html>
