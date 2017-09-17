7a0525c6ee98b71c3b8b90c715d44217eddf7846
rmit-s3490462-blake-cramer <s3490462@student.rmit.edu.au>
2017-09-17 22:44:33 +1000

    testing layouts again

--- resources/views/selectJob.blade.php
+++ resources/views/selectJob.blade.php
@@ -15,25 +15,29 @@
 		<h1>Select a Job</h1>	    
 		<div class="contentform">
 			<div class="leftcontact">	
-				<div class="form-group">
+                <div class="creatorField">
+                    <p>Seeker: </p>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" class="displayPic">
+                </div>
+				<div class="creatorField">
 					<p>Name: </p>	
 				</div>
-				<div class="form-group">
+				<div class="creatorField">
 					<p>Expertise: </p>	
 				</div>
-                <div class="form-group">
+                <div class="creatorField">
 					<p>Location: </p>	
 				</div>
             </div>
 
 			<div class="rightcontact">	
-				<div class="form-group">
+				<div class="creatorField">
 					<p>Price: </p>	
 				</div>
-				<div class="form-group">
+				<div class="creatorField">
 					<p>Deadline: </p>	
 				</div>
-                <div class="form-group">
+                <div class="creatorField">
 					<p>About The Job: </p>	
 				</div>
             </div>
