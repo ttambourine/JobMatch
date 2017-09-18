93d080224c37b209162054dfeb71e758045aeb27
BenjaminWalden <s3484942@student.rmit.edu.au>
2017-09-18 11:48:28 +1000

    prof edit

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -12,7 +12,7 @@
 		<h1>Kent Smuffman</h1>
 		<div class="middleProf">
 			<h3>Skills: </h3>
-			<h4>Transport</h4><h4>Accounting</h4><h4>Beauty Care</h4>
+			<h4>Transport</h4><h4>Accounting</h4><h5>Beauty Care</h5>
 			<h3>Parkdale VIC 3195, Australia</h3>
 			<h3>Member since 17th Septermber 2017</h3>
 
