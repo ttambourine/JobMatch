d63cb131ebb7db5e12311b9160ddc18117eca541
Ramadan Sali <dansali@users.noreply.github.com>
2017-09-20 16:05:28 +1000

    Fix structure & styling ??

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,88 +2,81 @@
 
 @section('pageTitle', 'Register')
 @section('content')
-  <form method="POST" action="{{ route('register') }}">
-	    {{ csrf_field() }}
-    <div class="contentformBox">	    
-    <div class="contentform">
-		<h1>Account Details</h1>
-    	<div class="leftcontact">
+	<form method="POST" action="{{ route('register') }}">
+		{{ csrf_field() }}
+	    <div class="contentformBox">	    
+		    <div class="contentform">
+				<h1>Account Details</h1>
+		    	<div class="leftcontact">            
+		            <div class="form-group">
+						<p>First Name<span>*</span></p>
+						<input type="text" name="fname" id="fname" required />
+					</div>
+		            
+					<div class="form-group">
+						<p>Last Name<span>*</span></p>
+						<input type="text" name="lname" id="lname" required />
+					</div>
 
-            
-            <div class="form-group">
-			<p>First Name<span>*</span></p>
-				<input type="text" name="fname" id="fname" required />
-			</div>
-            
-			<div class="form-group">
-			<p>Last Name<span>*</span></p>
-				<input type="text" name="lname" id="lname" required />
-			</div>
+					<div class="form-group">
+						<p>Email<span>*</span></p>
+		                <input type="email" name="email" id="email" required />
+					</div>
+					<div class="form-group">
+						<p>Password<span>*</span></p>
+		                <input type="password" name="password" id="password" required />
+					</div>
+					<div class="form-group">
+						<p>Confirm Password<span>*</span></p>
+		                <input type="password" name="passwordConf" id="passwordConf" required />
+					</div>
+				</div>
 
-			<div class="form-group">
-			<p>Email<span>*</span></p>
-                <input type="email" name="email" id="email" required />
-			</div>
-            <br>
-			<div class="form-group">
-			<p>Password<span>*</span></p>
-                <input type="password" name="password" id="password" required />
+				<div class="rightcontact">	
+					<div class="form-group">
+						<p>Contact No.<span>*</span></p>	
+						<input type="text" name="phone" id="phone" maxlength="10" required />
+					</div>
+		            <div class="form-group">
+						<p>Location<span>*</span></p>	
+						<input type="text" name="location" id="location" maxlength="20" required />
+					</div>
+					<div class="form-group">
+						<p>Skills 1/3<span>*</span></p>
+		                <select id="tags1">
+		                    <option value="" disabled="disabled" selected="selected">Please select an option</option>
+		                </select>
+					</div>
+					<div class="form-group">
+						<p>Skills 2/3</p>
+						<select id="tags2">
+		                <option value="" disabled="disabled" selected="selected">Please select an option</option>
+		            </select>
+					</div>
+					<div class="form-group">
+						<p>Skills 3/3</p>
+		                <select id="tags3">
+		                    <option value="" disabled="disabled" selected="selected">Please select an option</option>
+		                </select>
+					</div>
+		        </div>
 			</div>
 			<br>
-			<div class="form-group">
-			<p>Confirm Password<span>*</span></p>
-                <input type="password" name="passwordConf" id="passwordConf" required />
+	        <br>
+	        
+			<div class="centreAccount">
+				<div class="form-group">
+					<p>About You<span>* 250 characters or less</span></p>
+	                <textarea type="text" name="about" id="about" maxlength="250" placeholder="Share your story!"></textarea>
+				</div>
+			<br>
+			<input type="submit" value="Save Information" class="submitAccForm">
+			<br>
+			<br>
 			</div>
-			</div>
-
-		<div class="rightcontact">	
-
-			<div class="form-group">
-				<p>Contact No.<span>*</span></p>	
-				<input type="text" name="phone" id="phone" maxlength="10" required />
-			</div>
-        
-            <div class="form-group">
-				<p>Location<span>*</span></p>	
-				<input type="text" name="location" id="location" maxlength="20" required />
-			</div>
-			<div class="form-group">
-				<p>Skills 1/3<span>*</span></p>
-                <select id="tags1">
-                    <option value="" disabled="disabled" selected="selected">Please select an option</option>
-                </select>
-			</div>
-			<div class="form-group">
-				<p>Skills 2/3</p>
-				<select id="tags2">
-                <option value="" disabled="disabled" selected="selected">Please select an option</option>
-            </select>
-			</div>
-			<div class="form-group">
-				<p>Skills 3/3</p>
-                <select id="tags3">
-                    <option value="" disabled="disabled" selected="selected">Please select an option</option>
-                </select>
-			</div>
-        </div>
+		
 		</div>
-		<br>
-        <br>
-        
-		<div class="centreAccount">
-			<div class="form-group">
-				<p>About You<span>* 250 characters or less</span></p>
-                <textarea type="text" name="about" id="about" maxlength="250" placeholder="Share your story!"></textarea>
-			</div>
-		<br>
-		<input type="submit" value="Save Information" class="submitAccForm">
-		<br>
-		<br>
-		</div>
-	
-	</div>
-	</div>
-	
+		
 	</form>
 
 	<script>
