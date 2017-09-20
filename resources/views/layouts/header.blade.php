d24101761599834893d230dec4baac9f0e74f6fd
Ramadan Sali <dansali@users.noreply.github.com>
2017-09-20 15:59:36 +1000

    jquery

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,3 +8,7 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ URL::asset('resources/assets/css/style.css') }}">
+<script
+src="https://code.jquery.com/jquery-2.2.4.min.js"
+integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+crossorigin="anonymous"></script>
