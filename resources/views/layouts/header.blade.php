520ff05f508c5e9915e56184819cd579716b30fe
Ramadan Sali <s3512479@student.rmit.edu.au>
2017-09-22 06:22:23 +1000

    jquery ui css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,3 +12,4 @@
 src="https://code.jquery.com/jquery-2.2.4.min.js"
 integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
 crossorigin="anonymous"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
