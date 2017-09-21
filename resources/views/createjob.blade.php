eb0ee1f305909f5818e0add7229221a97065ad93
Ramadan Sali <s3512479@student.rmit.edu.au>
2017-09-22 06:14:26 +1000

    make deadline a date type

--- resources/views/createjob.blade.php
+++ resources/views/createjob.blade.php
@@ -23,7 +23,7 @@
 						</div>
 						<div class="form-group">
 							<p>Deadline</p>	
-							<input type="text" name="deadline" id="deadline" required />
+							<input type="date" name="deadline" id="deadline" required />
 						</div>
 					</div>
 
