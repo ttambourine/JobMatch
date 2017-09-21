a40973b8edee820aa15d40548f459e7058072572
Ramadan Sali <s3512479@student.rmit.edu.au>
2017-09-22 08:12:00 +1000

    wrong file shit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-use App\Tag;
-
 class LoginController extends Controller
 {
     /*
