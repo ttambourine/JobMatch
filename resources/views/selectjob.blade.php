5211800922676e46f8ab5db22cac45e86ec4e2f7
G-Force <ruzzah.is@gmail.com>
2017-10-04 22:39:19 +1100

    debugging why data isnt working from api.

--- resources/views/selectjob.blade.php
+++ resources/views/selectjob.blade.php
@@ -65,7 +65,7 @@
 	    }
 
 	    $.getJSON( "api/job_info/"+getUrlParameters("id"), function( data ) {
-	    	data = data[0];
+	    	alert(data);
 			$("#title").html(data.title);
 			$("#price").html(data.amount);
 			$("#deadline").html(data.due_date);
