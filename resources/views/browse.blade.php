ceccf348d12bdad78eac17d9c7e7a87e442773a5
G-Force <ruzzah.is@gmail.com>
2017-10-04 21:54:26 +1100

    typo & comment out template

--- resources/views/browse.blade.php
+++ resources/views/browse.blade.php
@@ -3,7 +3,7 @@
 @section('pageTitle', 'Browse')
 @section('content')
 	<div id="jobs">
-		<div class="jobTab">
+		<!--<div class="jobTab">
 			<div class="jobImg">
 				<img class="jobPic" src="https://albertaventure.com/wp-content/uploads/2014/12/01_bpoy_story01.jpg" width="100" height="100">
 			</div>
@@ -29,7 +29,7 @@
 				<h4> Match </h4>
 			</div>
 		</div>
-	</div>
+	</div>-->
 	
 	<div class="bink"></div>
 
@@ -37,7 +37,7 @@
 		$(document).ready(function(){
 			$.getJSON( "api/list_jobs", function( data ) {
 				for (var i = 0; i < data.length; i++){
-			 		var html = "<div class='jobTag'><div class='jobBody'><h3>"+data[i].title+"</h3><h4>Due: "+data[i].due_date+"</div><div class='jobEarn'><h1>$"+data[i].amount+"</h1></div></div>"
+			 		var html = "<div class='jobTab'><div class='jobBody'><h3>"+data[i].title+"</h3><h4>Due: "+data[i].due_date+"</div><div class='jobEarn'><h1>$"+data[i].amount+"</h1></div></div>"
 			 		$("#jobs").append(html);
 			 	}
 			});
