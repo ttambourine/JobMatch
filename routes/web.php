2fc97913cc4c632755c94bb573f0b61d788b7f88
Dan <s3512479@student.rmit.edu.au>
2017-10-06 08:04:40 +1100

    typo~

--- routes/web.php
+++ routes/web.php
@@ -129,7 +129,7 @@ Route::middleware('auth')->get('/api/get_matches/{id}', function($id) {
 		$finalJobs = json_decode(json_encode($finalJobs), True);
 		if($id == "1") {
 			//print_r($finalJobs);
-			$finalJobs = array_sort_($finalJobs, 'amount',);
+			$finalJobs = array_sort_($finalJobs, 'amount');
 			//print("\n");
 			//print_r($finalJobs);
 			//print("AMOUNT");
