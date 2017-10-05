a26a60f65d0bf79eb1a16e7aef0a62b22bd12a1f
Dan <s3512479@student.rmit.edu.au>
2017-10-06 06:29:11 +1100

    typo

--- routes/web.php
+++ routes/web.php
@@ -71,7 +71,7 @@ Route::middleware('auth')->post('update_acc', function(Request $request){
     ]);
 
     User::find( Auth::user()->id )->update( $request->all() );
-    return Route::to('preferences')->with('success', 'Data updated successfully');
+    return Redirect::to('preferences')->with('success', 'Data updated successfully');
 });
 Route::middleware('auth')->get('/api/get_matches/{id}', function($id) {
 
