02fd682e9d57ee94f299dc04a2f23b029ae1e406
rmit-s3490462-blake-cramer <s3490462@student.rmit.edu.au>
2017-10-09 21:58:26 +1100

    hmnmm

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,26 +2,55 @@
 
 @section('pageTitle', 'Profile')
 @section('content')
-	<div class="thinBanner">
-		<img src="http://i.imgur.com/JKdd1ns.jpg" class="wideBan">
-	</div>
+	<br/><br/><br/><br/><br/><br/>
 	<div class="profileImg">
 		<img src="https://albertaventure.com/wp-content/uploads/2014/12/01_bpoy_story01.jpg" width="200" height="200" class="profileImgPic">
 	</div>
 	<div class="mainProf">
-		<h1>Kent Smuffman</h1>
+		<h1 id="name"></h1>
 		<div class="middleProf">
 			<h3>Skills: </h3>
-			<h4>Transport</h4><h4>Accounting</h4><h5>Beauty Care</h5>
-			<h3>Parkdale VIC 3195, Australia</h3>
-			<h3>Member since 17th Septermber 2017</h3>
+			<h4 id="tags1">Loading</h4><h4 id="tags2"></h4><h5 id="tags3"></h5>
+			<h3 id="address">LLOAODOADOAIDNGINGING</h3>
+			<!--<h3>Member since 17th Septermber 2017</h3>-->
 
 		</div>
-		<h5>Report this member</h5> 
-		<div class="quote">Request a quote </div>
 	</div>
+<<<<<<< HEAD
     <div class="jobLinks">
         <a href="postedjob.blade.php">Jobs You Have Posted</a>
         <a href="appliedjobs.blade.php">Jobs You Have Applied For</a>
     </div>
+=======
+
+	<script>
+		var tag_names = [];
+	    $.getJSON( "api/list_tags", function( data ) {
+			for (var i = 0; i < data.length; i++){
+				tag_names[data[i].id] = data[i].name;
+			}
+			$.getJSON( "api/get_info", function( data ) {
+				$("#name").html(data.fname + " " + data.lname);
+				//$("#email").val(data.email);
+				//$("#about").val(data.about);
+				//$("#mobile").val(data.mobile);
+				$("#address").html(data.address);
+				//$("#lat").val(data.lat);
+				//$("#lng").val(data.lng);
+
+				if (data.tag1 != 0) {
+					$('#tags1').html(tag_names[data.tag1]);
+				}
+
+				if (data.tag2 != 0 && data.tag2 != null) {
+					$('#tags2').html(tag_names[data.tag2]);
+				}
+
+				if (data.tag3 != 0 && data.tag3 != null) {
+					$('#tags3').html(tag_names[data.tag3]);
+				}
+			});
+		});
+	</script>
+>>>>>>> 22d51f5cbc08f6e10598105af418c735bcaf4c5b
 @stop
