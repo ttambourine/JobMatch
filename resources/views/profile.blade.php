a2648fd7e4ef712242e200a00eec6959ff70c434
rmit-s3490462-blake-cramer <s3490462@student.rmit.edu.au>
2017-10-10 20:18:12 +1100

    spacing adjustment

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -46,5 +46,5 @@
 			});
 		});
 	</script>
-    <a href="appliedjobs.blade.php">View jobs you have applied for!</a> &nbsp; &nbsp;| <a href="postedjobs.blade.php">View jobs you have posted!</a>
+    <a href="appliedjobs.blade.php">View jobs you have applied for!</a> &nbsp; | <a href="postedjobs.blade.php">View jobs you have posted!</a>
 @stop
